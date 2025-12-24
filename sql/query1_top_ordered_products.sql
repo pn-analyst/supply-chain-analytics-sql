a8891422ed8626d35fa65f3e489923faa1187b2c
Pubudu <Analyst.nz@outlook.com>
2025-12-25 08:10:29 +1300

    Format SQL query for top ordered products

--- sql/query1_top_ordered_products.sql
+++ sql/query1_top_ordered_products.sql
@@ -1,5 +1,5 @@
--- Query 1: Top Ordered Products
-SELECT "Product ID", COUNT(*) AS order_count
+SELECT "Product ID",
+       COUNT(*) AS order_count
 FROM orders
 GROUP BY "Product ID"
 ORDER BY order_count DESC;
