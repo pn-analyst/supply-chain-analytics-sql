a8e872b840c5cebfb3cb1591eaa917d580d1b46e
Pubudu <Analyst.nz@outlook.com>
2025-12-25 08:11:41 +1300

    Fix SQL query to correctly format order date

--- sql/query3_monthly_order_trend.sql
+++ sql/query3_monthly_order_trend.sql
@@ -1,5 +1,6 @@
-SELECT strftime('%Y-%m', "order_date") AS month,
-       COUNT(*) AS order_count
+SELECT 
+    substr("Order Date", 7, 4) || '-' || substr("Order Date", 4, 2) AS month,
+    COUNT(*) AS order_count
 FROM orders
 GROUP BY month
 ORDER BY month;
