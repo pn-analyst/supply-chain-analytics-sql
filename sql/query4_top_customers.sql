fcc1a8560ee51b7d89fc5c7a1e41f13b8dc4e611
Pubudu <Analyst.nz@outlook.com>
2025-12-25 08:44:08 +1300

    Fix SQL syntax for top customers query

--- sql/query4_top_customers.sql
+++ sql/query4_top_customers.sql
@@ -1,6 +1,7 @@
-SELECT "Customer",
-       COUNT(*) AS total_orders
+SELECT 
+    Customer,
+    COUNT(*) AS total_orders
 FROM orders
-GROUP BY "Customer"
+GROUP BY Customer
 ORDER BY total_orders DESC
 LIMIT 10;
