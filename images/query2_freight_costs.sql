3cce3563f1957b6a46320ee965d456def9c7385f
Pubudu <Analyst.nz@outlook.com>
2025-12-24 23:54:34 +1300

    Delete images/query2_freight_costs.sql

--- images/query2_freight_costs.sql
+++
@@ -1,6 +0,0 @@
-SELECT "orig_port_cd",
-       "dest_port_cd",
-       AVG(CAST(REPLACE("rate", '$', '') AS REAL)) AS avg_rate
-FROM freight_rates
-GROUP BY "orig_port_cd", "dest_port_cd"
-ORDER BY avg_rate DESC;
